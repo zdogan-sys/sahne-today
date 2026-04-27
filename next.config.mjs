dba1825f5d584c85205d01af56ccf66bf9889bdf
zdogan-sys <z_dogan@hotmail.com>
2026-04-27 20:35:00 +0300

    fix: build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
